f9220954f573afc77f2a0f8fdf39fadb0b438651
YevhenDenyk <denyk.yevhen@gmail.com>
2022-09-24 12:10:49 +0300

    react-hook-form

--- src/components/CarForm/CarForm.js
+++ src/components/CarForm/CarForm.js
@@ -1,8 +1,29 @@
+import {useForm} from "react-hook-form";
+
 const CarForm = () => {
+    const {register, handleSubmit, reset, formState: {errors, isValid}, setValue} = useForm({mode: 'all'});
+
+    const submit = (car) => {
+
+    };
+
     return (
-        <div>
-            
-        </div>
+        // <form onSubmit={handleSubmit(submit)} onChange={() => console.log(errors)}>
+        //     <input type="text" placeholder={'model'}{...register('model', {
+        //         required: true,
+        //         minLength: {value: 4, message: 'min 4 ch'}
+        //     })}/>
+        //     {errors.model&&<span>{errors.model.message}</span>}
+        //     <input type="text" placeholder={'price'}{...register('price', {valueAsNumber: true})}/>
+        //     <input type="text" placeholder={'year'}{...register('year', {valueAsNumber: true})}/>
+        //     <button disabled={!isValid}>Save</button>
+        // </form>
+        <form onSubmit={handleSubmit(submit)}>
+            <input type="text" placeholder={'model'}{...register('model')}/>
+            <input type="text" placeholder={'price'}{...register('price')}/>
+            <input type="text" placeholder={'year'}{...register('year')}/>
+            <button disabled={!isValid}>Save</button>
+        </form>
     );
 };
 
