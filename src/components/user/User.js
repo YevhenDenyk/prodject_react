f68ae4dc78658e012e1023b778a441cc3c2f49af
YevhenDenyk <denyk.yevhen@gmail.com>
2022-09-26 20:21:42 +0300

    l1 v2

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -2,11 +2,10 @@ const User = ({user}) => {
 
     return (
         <div>
-            <h3>id:{user.id} name:{user.name}</h3>
+            <h3>id:{user.id} name: {user.name}</h3>
             <img src={user.image} alt={user.name}/>
         </div>
     );
 };
-
 
 export {User};
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,13 +3,15 @@ import {useEffect, useState} from "react";
 import {User} from "../user/User";
 
 const Users = () => {
-    const {users, setUsers} = useState([]);
+
+    const [users, setUsers] = useState([]);
 
     useEffect(()=>{
         fetch('https://rickandmortyapi.com/api/character')
             .then(value => value.json())
-            .then(value => {setUsers(value.results)});
-
+            .then(value => {
+                setUsers(value.results)
+            });
     },[])
 
     return (
